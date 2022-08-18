af0106696dfd80928febca1fadbcc10413de98ab
Aditya Lagad <aadityalagad@gmail.com>
2022-08-18 16:15:41 +0530

    Created a responsive Main component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import "./App.css";
-import Header from "./Header";
+import Main from "./Main";
 
 function App() {
     return <div className="app">
-        <Header />
+        <Main />
     </div>;
 }
 
